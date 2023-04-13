9f440f533d340b8b8e91c2d3234963712ebae680
TstroheckerClearfcu <66829920+TstroheckerClearfcu@users.noreply.github.com>
2023-04-13 11:24:13 -0400

    Add files via upload

---
+++ Script/FieldLocations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPX_File_Script
+{
+    class FieldLocations
+    {
+        public int accountLoc { get; set; } = -1;
+        public int dateLoc { get; set; } = -1;
+        public int paymentAmountLoc { get; set; } = -1;
+        public int feeAmountLoc { get; set; } = -1;
+        public int statusLoc { get; set; } = -1;
+        public int paymentTypeLoc { get; set; } = -1;
+    }
+}
---
+++ Script/Totals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPX_File_Script
+{
+    class Totals
+    {
+        public double visatot { get; set; } = 0.00;
+        public double CCtotal { get; set; } = 0.00;
+        public double CCFee { get; set; } = 0.00;
+        public double visafee { get; set; } = 0.00;
+        public double SLTot { get; set; } = 0.00;
+        public double SLFee { get; set; } = 0.00;
+        public int VisaCount { get; set; } = 0;
+        public double VisaAmount { get; set; } = 0.00;
+        public int OldVisaCount { get; set; } = 0;
+        public double OldVisaAmount { get; set; } = 0.00;
+    }
+}
